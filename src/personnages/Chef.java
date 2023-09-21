97825d4e7134e56920e12bb0c371890168f18ffd
Dadou Assogba <ssd2597a@ens.fsi>
2023-09-21 12:02:03 +0200

    terminer

---
+++ src/personnages/Chef.java
@@ -0,0 +1,27 @@
+package personnages;
+
+public class Chef {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+
+	public Chef(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+		this.effetPotion = effetPotion;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+	private String prendreParole() {
+		return "Le chef " + nom + " du village " ;
+	}
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " +romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}
+}
